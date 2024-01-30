9af210aa5d8585ba9ccdd9436fa2189f71d8a376
ducnguyen <levinguyen.dl@gmail.com>
2024-01-31 00:39:09 +0700

    fix: rebuild command

--- shell/scripts/rebuild.nix
+++ shell/scripts/rebuild.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   rebuild = pkgs.writeShellScriptBin "rebuild" ''
-    nix-on-droid switch --flake ~/#default
+    nix-on-droid switch --flake ~/nix-on-droid#default
   '';
 in {
   home.packages = [rebuild];
